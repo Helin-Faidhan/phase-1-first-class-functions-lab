f3fcee3e753057de8902a04079af23451d947661
Helin Faidhan <helinkurd1997@gmail.com>
2022-10-27 20:42:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Code your solution in this file!
+const Drivers=['ant', 'bison', 'camel', 'duck', 'elephant']
+
+
+    const returnFirstTwoDrivers= function(array){
+        return array.slice(0,2);
+    }
+
+console.log(returnFirstTwoDrivers(Drivers))
+
+
+
+const azad=['ant', 'bison', 'camel', 'duck', 'elephant']
+
+
+    const returnLastTwoDrivers= function(last){
+        return last.slice(-2);
+    }
+
+console.log(returnLastTwoDrivers(azad))
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers ]
+
+function createFareMultiplier(multiplier){
+    return function (value){
+        return multiplier*value
+
+    }
+
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+console.log(fareDoubler)
+
+const fareTripler = createFareMultiplier(3)
+
+console.log(fareTripler)
+
+function selectDifferentDrivers(){
+    const drivers=[returnFirstTwoDrivers,returnLastTwoDrivers ]
+    return drivers
+
+}
+
+
+
+
+
+
+
+
+
+
+
